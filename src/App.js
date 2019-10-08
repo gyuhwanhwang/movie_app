813be009feaefc56bfe19020c7a41f0b455f6d4a
gyuhwanhwang <rbghks0327@gmail.com>
2019-10-08 20:00:55 +0900

    #3.0  Class Componets and State

--- src/App.js
+++ src/App.js
@@ -1,71 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image:
-            "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg",
-        rating: 5
-    },
-    {
-        id: 2,
-        name: "Samgyeopsal",
-        image:
-            "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG",
-        rating: 4.9
-    },
-    {
-        id: 3,
-        name: "Bibimbap",
-        image:
-            "http://recipe1.ezmember.co.kr/cache/recipe/2018/01/18/91237b8fa18b78700d6586e1c6c9f73b1.jpg",
-        rating: 4.8
-    },
-    {
-        id: 4,
-        name: "Doncasu",
-        image:
-            "http://recipe1.ezmember.co.kr/cache/recipe/2019/01/12/b9343d314206275c1b6d0d0c4fcc2ce71.jpg",
-        rating: 5.5
+// 기초는 React.Componet 에서 extends로 가져온다.
+// App은 React,componet에서 확장된 것임
+// react는 자동적으로 모든 class component의 render method를 실행하고자 함
+class App extends React.Component {
+    // Conponet의 data를 바꾸고 싶어서 class Componet를 사용
+    // state에 우리가 바꾸고 싶은 data를 넣자
+    state = {
+        count: 0
+    };
+    add = () => {
+        console.log("add");
+    };
+    minus = () => {
+        console.log("minus");
+    };
+    render() {
+        // class componet이기 때문에 this 명시
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                {/* react에서 button은 onClick을 기본적 prop로 가지고 있다 */}
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-];
-
-// props.name 와 {} 내부에 fav를 쓰는 것은 같다
-function Food({ name, picture, rating }) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <h4>{rating}/5.0</h4>
-            <img src={picture} alt={name} />
-        </div>
-    );
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-};
-
-function App() {
-    // props는 argument로 간다
-    return (
-        <div>
-            {/* dish는 object다! */}
-            {/*  map함수는 배열의 각 item을 한번씩 돌면서 실행되고 return 모아서 다시 배열을 return */}
-            {foodILike.map(dish => (
-                // key는 componet에 전달되지 않고 react내부에서 사용
-                <Food
-                    key={dish.id}
-                    name={dish.name}
-                    picture={dish.image}
-                    rating={dish.rating}
-                />
-            ))}
-        </div>
-    );
 }
 
 export default App;
