c5bfe249a8942f9bddb379ca7f1ae1e31787951b
gyuhwanhwang <rbghks0327@gmail.com>
2019-10-09 00:28:57 +0900

    Updates

---
+++ precache-manifest.53d6a63c742057e7d40a464abaee9e6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe2872f072574d7c0ba5c89b3d1d1f7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b93e6cf0a5be5993dbe3",
+    "url": "/movie_app/static/css/main.154cb2c3.chunk.css"
+  },
+  {
+    "revision": "dd26cfe0aeac87999839",
+    "url": "/movie_app/static/js/2.6e26b474.chunk.js"
+  },
+  {
+    "revision": "b93e6cf0a5be5993dbe3",
+    "url": "/movie_app/static/js/main.0d57527c.chunk.js"
+  },
+  {
+    "revision": "9b07edc508fa4bfefff7",
+    "url": "/movie_app/static/js/runtime-main.ab878cc8.js"
+  }
+]);
